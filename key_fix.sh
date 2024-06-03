80d51400287ae97783b3eb882f8f517fcaf7fcf0
xah96 <102232889+xah69@users.noreply.github.com>
2024-06-03 09:47:48 +0530

    v-0.1
    v-0.1

---
+++ key_fix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source key_fix_config.sh
+
+arg_var=$1
+
+if [ "$arg_var" = "off" ]; then
+    echo "[Killing Keyboard Off]"
+    xinput float $slave_id
+elif [ "$arg_var" = "on" ]; then
+    echo "[Turning Keyboard On]"
+    xinput reattach $slave_id $master_id
+elif [ "$arg_var" = "show" ]; then
+    echo "[showing list of devices and its status]"
+    xinput list
+else
+    echo "[choose the device below to Turn it On or Off]"
+    echo "[usage]"
+    echo "$ ./key_fix.sh on"
+    echo "$ ./key_fix.sh off"
+    echo "$ ./key_fix.sh show"
+    echo "# [note] --> You need provide <master id> and <slave id> in key_fix_config.sh"
+    xinput list
+fi
---
+++ key_fix_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+slave_id="12"
+master_id="3"
